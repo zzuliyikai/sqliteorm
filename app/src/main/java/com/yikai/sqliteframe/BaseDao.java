cccd1393612b1ce2a16e93d2284fec449db2a714
yikai <15738800415@163.com>
2019-04-11 15:42:30 +0800

    创建表的相关优化

--- app/src/main/java/com/yikai/sqliteframe/BaseDao.java
+++ app/src/main/java/com/yikai/sqliteframe/BaseDao.java
@@ -1,5 +1,6 @@
 package com.yikai.sqliteframe;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.yikai.sqliteframe.annotation.DbField;
@@ -12,12 +13,13 @@ import java.util.HashMap;
  * Created by Administrator on 2019/4/8.
  */
 
-public class BaseDao<T> implements IBaseDao<T> {
+public abstract class BaseDao<T> implements IBaseDao<T> {
 
     private Class<T> mEntityClass;
     private SQLiteDatabase mSqLiteDatabase;
     private String mTableName;
-    private HashMap<String,Field> hashMap;
+    //存放表的列名和类的属性
+    private HashMap<String, Field> mCacheMap;
 
 
     private boolean isInit = false;
@@ -26,26 +28,116 @@ public class BaseDao<T> implements IBaseDao<T> {
         if (!isInit) {
             this.mEntityClass = entityClass;
             this.mSqLiteDatabase = sqLiteDatabase;
-            hashMap = new HashMap<>();
 
             mTableName = mEntityClass.getAnnotation(DbTable.class).value();
+
+            if (mTableName == null) {
+                mTableName = mEntityClass.getSimpleName();
+            }
 
             //判断数据库是否打开，如果没有打开直接初始化失败
             if (!mSqLiteDatabase.isOpen()) {
                 return false;
             }
-            //创建表是否成功，如果不成功直接返回false
-            if (!isCreateTable()) {
-                return false;
+
+            //创建数据库表
+            if (isCreateTable()) {
+                mCacheMap = new HashMap<>();
+                //查看数据库表
+                initCacheMap();
+                isInit = true;
             }
 
-            //查看数据库表
-
-
-            isInit = true;
         }
         return isInit;
     }
+
+    private void initCacheMap() {
+
+        String sql = "select * from " + mTableName + " limit 1,0";
+
+        Cursor cursor = mSqLiteDatabase.rawQuery(sql, null);
+
+        String[] columnNames = cursor.getColumnNames();
+        Field[] fields = mEntityClass.getFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+        }
+
+
+        for (String columnName : columnNames) {
+
+            Field colmunFilde = null;
+
+            for (Field field : fields) {
+                String value;
+                if (field.getAnnotation(DbField.class).value() == null) {
+                    value = field.getName();
+                } else {
+                    value = field.getAnnotation(DbField.class).value();
+                }
+                if (columnName.equals(value)) {
+                    colmunFilde = field;
+                    break;
+                }
+            }
+
+            if (colmunFilde != null) {
+                mCacheMap.put(columnName, colmunFilde);
+            }
+
+        }
+        cursor.close();
+    }
+
+    public boolean isCreateTable() {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("create table if not exists ");
+        stringBuffer.append(mTableName + " (");
+        Field[] fields = mEntityClass.getFields();
+
+        for (Field field : fields) {
+            Class<?> type = field.getType();
+            if (type == String.class) {
+                String value = field.getAnnotation(DbField.class).value();
+                stringBuffer.append(value + " TEXT,");
+            } else if (type == Double.class) {
+                String value = field.getAnnotation(DbField.class).value();
+                stringBuffer.append(value + " DOUBLE,");
+            } else if (type == Integer.class) {
+                String value = field.getAnnotation(DbField.class).value();
+                stringBuffer.append(value + " INTEGER,");
+            } else if (type == Long.class) {
+                String value = field.getAnnotation(DbField.class).value();
+                stringBuffer.append(value + " BIGINT,");
+            } else if (type == Byte[].class) {
+                String value = field.getAnnotation(DbField.class).value();
+                stringBuffer.append(value + " BLOB,");
+            } else {
+                /**
+                 * 暂时不支持
+                 */
+                continue;
+            }
+        }
+        if (stringBuffer.charAt(stringBuffer.length() - 1) == ',') {
+
+            stringBuffer.deleteCharAt(stringBuffer.length() - 1);
+        }
+        stringBuffer.append(")");
+        try {
+
+            mSqLiteDatabase.execSQL(stringBuffer.toString());
+
+        } catch (Exception e) {
+            return false;
+        }
+
+
+        return true;
+    }
+
 
     @Override
     public long insert(T entity) {
@@ -53,40 +145,4 @@ public class BaseDao<T> implements IBaseDao<T> {
         return 0;
     }
 
-    public boolean isCreateTable() {
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("create table if not exists ");
-        stringBuffer.append(mTableName +" (");
-        Field[] fields = mEntityClass.getFields();
-
-        for (Field field :fields) {
-            Class<?> type = field.getType();
-            if (type == String.class){
-                String value = field.getAnnotation(DbField.class).value();
-                stringBuffer.append(value + " TEXT,");
-            }else if (type == Double.class){
-                String value = field.getAnnotation(DbField.class).value();
-                stringBuffer.append(value + " DOUBLE,");
-            }else if (type == Integer.class){
-                String value = field.getAnnotation(DbField.class).value();
-                stringBuffer.append(value + " INTEGER,");
-            }else if (type == Long.class){
-                String value = field.getAnnotation(DbField.class).value();
-                stringBuffer.append(value + " BIGINT,");
-            }else if (type == Byte[].class){
-                String value = field.getAnnotation(DbField.class).value();
-                stringBuffer.append(value + " BLOB,");
-            }else {
-
-                /**
-                 * 暂时不支持
-                 */
-                continue;
-            }
-        }
-
-
-
-        return false;
-    }
 }
